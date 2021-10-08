b4ae3b799e11d9670293c5b3cb513258ce1c25d0
최형욱 <gladnxm@gmail.com>
2021-10-08 19:44:46 +0900

    드랍다운을 메인메뉴 안에 넣어서 자스없이 hover만으로 구현

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-const coffeeMainMenuEl = document.querySelector(".main-menu .coffee");
-const coffeeDropdownEl = document.querySelector(".coffee.dropdown");
-function coffee_dropdown_show() {
-  coffeeDropdownEl.style.visibility="visible";
-}
-function coffee_dropdown_hide() {
-  coffeeDropdownEl.style.visibility="hidden";
-}
-
-const menuMainMenuEl = document.querySelector(".main-menu .menu");
-const menuDropdownEl = document.querySelector(".menu.dropdown");
-function menu_dropdown_show() {
-  menuDropdownEl.style.visibility="visible";
-}
-function menu_dropdown_hide() {
-  menuDropdownEl.style.visibility="hidden";
-}
-
-const storeMainMenuEl = document.querySelector(".main-menu .store");
-const storeDropdownEl = document.querySelector(".store.dropdown");
-function store_dropdown_show() {
-  storeDropdownEl.style.visibility="visible";
-}
-function store_dropdown_hide() {
-  storeDropdownEl.style.visibility="hidden";
-}
-
-
 // 검색창 
 const searchEl = document.querySelector(".search input");
 searchEl.addEventListener('focus', function() {
